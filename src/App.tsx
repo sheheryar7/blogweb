0ed2e1dc68e848a8e9ccf5ed167cf4c51b7e98da
sheheryar7 <sheheryarbangash71@gmail.com>
2022-10-28 00:38:43 +0500

    in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import "./App.css";
-import { Link } from "react-router-dom";
+
 import Main from "./pages/Main";
 
 function App() {
   return (
     <>
       <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-        </ul>
-        <hr />
         <Main />
       </div>
     </>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 const Home = () => {
-    return <p>hello</p>;
+    return (
+
+        <Container maxWidth= "xl" >
+        <Typography component="div" style={{ 
+          backgroundColor: 'lightseagreen', height: '100vh' 
+        }}>
+          
+        </Typography>
+      </Container>
+    )
+     
+        
 }
+
 export default Home;
