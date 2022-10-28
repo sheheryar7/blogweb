b221980700a38fa307a8343d7e005afbbb071767
sheheryar7 <sheheryarbangash71@gmail.com>
2022-10-28 21:38:27 +0500

    in progress

--- src/components/webFooter.tsx
+++ src/components/webFooter.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 const Webfooter = () => {
-    return (
+  return (
+    <>
+      <Container
+        style={{ maxWidth: "100em", height: "10em", backgroundColor: "blue" }}
+      >
+        <Container style={{display: "flex-start", backgroundColor: "green", height: "50px", width: "50px"}}>
 
-        <><Container style={{ maxWidth: "100em", height: "10em", backgroundColor: "blue" }}>
-        <Typography>
-          This is footer
+        </Container>
+      </Container>
+      <Container
+        style={{ maxWidth: "100em", height: "25px", backgroundColor: "black" }}
+      >
+        <Typography style={{ textAlign: "center", color: "white" }}>
+          Copyright@ WebBlog 2022
         </Typography>
-
-      </Container><Container style={{ maxWidth: "100em", height: "25px", backgroundColor: "black" }}>
-          <Typography style={{ textAlign: "center", color: "white" }}>Copyright@ WebBlog 2022</Typography>
-        </Container></>
-    )
-     
-        
-}
+      </Container>
+    </>
+  );
+};
 
 export default Webfooter;
